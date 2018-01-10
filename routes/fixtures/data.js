2bd2ec31655ca6d4fe5b3294f7befd3e364425f7
Manuel Roth <manuel.roth@nzz.ch>
2018-01-10 17:08:17 +0100

    Add more fixture data and fix styles for long labels

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -4,9 +4,13 @@ const fixtureDataDirectory = '../../resources/fixtures/data'
 // has to be in sync with files created in build task - see ../../tasks/build.js
 const fixtureData = [
   require(`${fixtureDataDirectory}/two-images.json`),
+  require(`${fixtureDataDirectory}/two-images-long-labels.json`),
   require(`${fixtureDataDirectory}/three-images.json`),
+  require(`${fixtureDataDirectory}/three-images-long-labels.json`),
   require(`${fixtureDataDirectory}/five-images.json`),
   require(`${fixtureDataDirectory}/five-images-long-labels.json`),
+  require(`${fixtureDataDirectory}/ten-images.json`),
+  require(`${fixtureDataDirectory}/ten-images-long-labels.json`)
 ]
 
 module.exports = {
