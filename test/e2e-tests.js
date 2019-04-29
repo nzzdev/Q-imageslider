a5b575f0a2fe8ab5579bd5c350ad5ec39dfe7d6c
Manuel Roth <manuel.roth@nzz.ch>
2019-04-29 17:50:25 +0200

    Use hapi org packages

--- test/e2e-tests.js
+++ test/e2e-tests.js
@@ -1,6 +1,6 @@
 const Lab = require("lab");
 const Code = require("code");
-const Hapi = require("hapi");
+const Hapi = require("@hapi/hapi");
 const lab = (exports.lab = Lab.script());
 
 const glob = require("glob");
@@ -22,7 +22,7 @@ before(async () => {
         cors: true
       }
     });
-    await server.register(require("inert"));
+    await server.register(require("@hapi/inert"));
     server.route(routes);
   } catch (err) {
     expect(err).to.not.exist();
